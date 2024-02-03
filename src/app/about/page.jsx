498d177528aa908bb3fddcd54a40b9b1a780c2a3
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-02-03 21:13:55 +0530

    about page and contact page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,38 @@
+import Image from 'next/image';
+import styles from './about.module.css';
+
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>We create digital ideas thar are bigger, bolder,braver and better</h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger,bolder, braver and better. we believe in good ideas flexibility and 
+          presission we are worlds our special Team best consultancy & finance solution provider, wide range of web and software development services
+
+        </p>
+        <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Years of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Years of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Years of experience</p>
+            </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src={'/about.png'} alt='About image' fill  className={styles.img}/>
+      </div>
+    </div>
   )
 }
 
-export default AboutPage
+export default AboutPage 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import styles from './footer.module.css';
+
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        logo
+      </div>
+      <div className={styles.text}>
+          Aswin creative thughts agency All right reserved
+      </div>
+    </div>
   )
-}
+} 
 
 export default Footer
